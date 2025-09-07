c90fbf508fe049ef6f23a69a81443c17cad3f810
rahhal10 <abdallahrahhal77@gmail.com>
2025-09-07 22:54:29 +0300

    Added a Login/Signup page and a working Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,43 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Routes/Home'
 import About from './Routes/About'
 import Footer from './Comp/Footer'
+import Navbar from './Comp/Navbar'
+import Login from './Routes/Login'
+import Signup from './Routes/Signup'
+import { useState } from 'react'
 
 function App() {
+
+   const [user, setUser] = useState(() => {
+      try {
+        const stored = localStorage.getItem('user');
+        return stored ? JSON.parse(stored) : null;
+      } catch (e) {
+        return null;
+      }
+    });
+
+  const onLogin = (loginData) => {
+    setUser(loginData);
+    console.log('Logged in user:', loginData);
+    localStorage.setItem('user', JSON.stringify(loginData));
+  };
+
+  const onLogout = () => {
+    setUser(null);
+    localStorage.removeItem('user');
+  };
 
 
   return (
     <>
     <Router>
+      <Navbar onLogout={onLogout} user={user} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login onLogin={onLogin} />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer />
     </Router>
